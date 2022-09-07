9562728819d241e9991cf27ea8b8bfd6e253a728
jeffcunat <jeanfrancois.cunat@orange.com>
2022-09-07 18:27:41 +0200

    Initial commit

---
+++ app.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const { data } = useFetch('https://api.chucknorris.io/jokes/random');
+</script>
+<template>
+  <div>
+    {{ data }}
+  </div>
+</template>
+<style>
+body {
+  background-color: #000;
+  color: #fff;
+}
+</style>
